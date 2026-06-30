f3c33dd5f44e30ba929bc710c0d00bc38bfb7771
Dipanshu Soni <dipanshusoni369@gmail.com>
2026-06-30 08:21:24 +0530

    Refactor server to use Node HTTP Server.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,6 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
 app.use(express.json());
+const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,7 +15,7 @@ const connectDB = require('./config/db');
 const startServer = async () => {
     await connectDB();
 
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
 };
@@ -24,10 +26,10 @@ const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
 const userRoutes = require("./routes/userRoutes");
-app.use('/api', userRoutes);
+app.use('/api/users', userRoutes);
 
 const messageRoutes = require("./routes/messageRoutes");
-app.use("/api", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get('/', (req, res) => {
     res.send("ChatSphere server is running !");
