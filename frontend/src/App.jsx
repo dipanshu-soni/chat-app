b627f51e14b6d754e461f02b04318300e4d507f2
Dipanshu Soni <dipanshusoni369@gmail.com>
2026-07-01 23:29:39 +0530

    Implement JWT authentication and protected routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ProtectedRoute from './components/ProtectedRoutes';
 
 function App()
 {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>}
+      />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
